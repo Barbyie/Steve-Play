fab2b3010adbe4e131a0ab2d932513474d462448
Thiago Barbieri Catarina <thiago.barbieri2002@gmail.com>
2024-01-02 22:27:20 -0300

    Migrated from discord.py to nextcord

--- main2.py
+++ main2.py
@@ -1,10 +1,12 @@
-import discord
-from discord.ext import commands
-from discord import FFmpegPCMAudio
+import nextcord
+from nextcord.ext import commands
+from nextcord import FFmpegPCMAudio
 import os
+import youtube_dl
 
-intents = discord.Intents.default()
+intents = nextcord.Intents.default()
 intents.voice_states = True
+intents.message_content = True
 intents.members = True
 queues = {}
 
@@ -13,11 +15,19 @@ client = commands.Bot(command_prefix='!', intents=intents)
 with open('token.txt', 'r') as file:
     token = file.readline()
 
-def check_queue(ctx, id):
-    if queues[id] != []:
-        voice = ctx.guild.voice_client
-        source = queues[id].pop(0)
-        player = voice.play(source)
+async def check_queue(ctx, guild_id):
+    if queues[guild_id] is not None and queues[guild_id]:
+        voice = nextcord.utils.get(client.voice_clients, guild=ctx.guild)
+        if voice is not None and voice.is_playing():
+            return
+
+        source = queues[guild_id].pop(0)  # Retrieve the next source from the queue
+        try:
+            player = voice.play(source, after=lambda x=None: check_queue(ctx, guild_id))
+            await ctx.send(f"Playing **{source.title}**")  # Assuming source has a `title` attribute
+        except Exception as e:
+            await ctx.send("An error occurred while playing the audio: {}".format(e))
+
 def find_join_channel(guild):
     for channel in guild.text_channels:
         if channel.permissions_for(guild.me).send_messages and channel.permissions_for(guild.me).view_audit_log:
@@ -25,13 +35,13 @@ def find_join_channel(guild):
     return None
 
 def is_connected(ctx):
-    voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+    voice_client = nextcord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
     return voice_client and voice_client.is_connected()
 
 @client.event
 async def on_ready():
     print(f"Bot is ready")
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a bunch of songs"))
+    await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="a bunch of songs"))
 
 @client.command()
 async def hello(ctx):
@@ -55,11 +65,12 @@ async def on_member_remove(member):
 
 @client.command(pass_context=True)
 async def join(ctx):
+    print("We got here at least")
     if (ctx.author.voice):
         channel = ctx.message.author.voice.channel
         voice = await channel.connect()
         file = os.path.dirname(os.path.abspath(__file__))
-        source = FFmpegPCMAudio(f"{file}/lean.mp3")
+        source = FFmpegPCMAudio(f"{file}/song.mp3")
         player = voice.play(source)
     else:
         await ctx.send("You are not in a voice channel.")
@@ -67,14 +78,14 @@ async def join(ctx):
 @client.command(pass_context=True)
 async def leave(ctx):
     if ctx.voice_client:
-        await ctx.guild.voice_client.disconnect()
+        await ctx.guild.voice_client.disconnect(force=True)
         await ctx.send("Steve left the voice channel.")
     else:
         await ctx.send("Steve is not in a voice channel")
 
 @client.command(pass_context=True)
 async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice = nextcord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         voice.pause()
     else:
@@ -82,7 +93,7 @@ async def pause(ctx):
 
 @client.command(pass_context=True)
 async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice = nextcord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_paused():
         voice.resume()
     else:
@@ -90,35 +101,40 @@ async def resume(ctx):
 
 @client.command(pass_context=True)
 async def stop(ctx):
-    voice = discord.utils.get(client.voice_clients, guild= ctx.guild)
+    voice = nextcord.utils.get(client.voice_clients, guild= ctx.guild)
     voice.stop()
 
 @client.command(pass_context=True)
-async def play(ctx, arg):
-    if ctx.author.voice:
-        channel = ctx.message.author.voice.channel
-        print(is_connected(ctx))
-        if is_connected(ctx) != True:
-            voice = await channel.connect()
-        else:
-            voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+async def play(ctx, *, query):
+    ydl_opts = {'format': 'bestaudio/best', 'noplaylist': 'True'}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info = ydl.extract_info("ytsearch:{}".format(query), download=False)
+            url = info['entries'][0]['url']  # Use the first result
 
-    file = os.path.dirname(os.path.abspath(__file__))
-    source = FFmpegPCMAudio(f"{file}/{arg}.mp3")
-    player = voice.play(source, after=lambda x=None: check_queue(ctx, ctx.message.guild.id))
+            # Extract audio using youtube-dl
+            info = ydl.extract_info(url, download=False)
+            source = FFmpegPCMAudio(info['url'])
+        except Exception as e:
+            print(e)
+            return
 
-@client.command(pass_context=True)
-async def queue(ctx, arg):
-    voice = ctx.guild.voice_client
-    file = os.path.dirname(os.path.abspath(__file__))
-    source = FFmpegPCMAudio(f"{file}/{arg}.mp3")
-    guild_id = ctx.message.guild.id
-    if guild_id in queues:
-        queues[guild_id].append(source)
+        guild_id = ctx.message.guild.id
+        voice = nextcord.utils.get(client.voice_clients, guild=ctx.guild)
 
-    else:
-        queues[guild_id] = [source]
+        if not voice:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+                voice = nextcord.utils.get(client.voice_clients, guild=ctx.guild)
+            else:
+                await ctx.send("You are not in a voice channel.")
+                return
 
-    await ctx.send("Song added to the queue.")
+        try:
+            player = voice.play(source, after=lambda x=None: check_queue(ctx, guild_id))
+            await ctx.send(f"Playing **{query}**")
+        except Exception as e:
+            await ctx.send("An error occurred while playing the audio: {}".format(e))
+
 
 client.run(token)
